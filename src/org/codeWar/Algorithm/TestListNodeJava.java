d4a548f9764840d7fdb01a266d28c6f6ec6f3716
Utowoo <qian.wu7300@gmail.com>
2018-07-02 22:36:56 +0800

    TestListNodeJava
    Add

--- src/org/codeWar/Algorithm/TestListNodeJava.java
+++ src/org/codeWar/Algorithm/TestListNodeJava.java
@@ -1,4 +1,12 @@
 package org.codeWar.Algorithm;
 
+import org.codeWar.DataStructure.ListNode;
+
 public class TestListNodeJava {
+   public static void main(String arg[]){
+       ListNode a = new ListNode(1);
+       a.next = new ListNode(2);
+
+       System.out.println(a.toString());
+   }
 }
